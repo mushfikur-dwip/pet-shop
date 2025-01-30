9eea5dd35b15b5b3e8863ba13f1256b7b8b26790
mushfikur-dwip <musfikurrahmandip@gmail.com>
2025-01-31 02:07:52 +0600

    Hero section done with responsibale

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ["Sora", "serif"],
+        adlam: ["ADLaM Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
